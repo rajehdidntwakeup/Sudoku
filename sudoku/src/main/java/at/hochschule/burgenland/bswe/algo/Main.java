e4ddd495e3964a98e48d0b59e4b0ffbd8697c550
rajehdidntwakeup <84966465+rajehdidntwakeup@users.noreply.github.com>
2025-09-06 18:10:14 +0200

    Add files via upload

---
+++ sudoku/src/main/java/at/hochschule/burgenland/bswe/algo/Main.java
@@ -0,0 +1,12 @@
+package at.hochschule.burgenland.bswe.algo;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       UiMenu uiMenu = new UiMenu();
+
+       uiMenu.showMenu();
+    }
+
+}
